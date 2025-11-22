37a95d2f199291476cb487756a8c5225a2197c75
Whendell <whndllsnts@gmail.com>
2025-11-22 23:16:27 +0800

    Implement count logging: Append the current counter value to the 'Previous entires' history element (save-el).

--- JavaScript Fundamentals/index.js
+++ JavaScript Fundamentals/index.js
@@ -1,12 +1,13 @@
-let countEl = document.getElementById('count-el');
-
-let count = 0;
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
 function increment() {
-    count = count + 1;
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
-    console.log(count);
+    let countStr = count + " - "
+    saveEl.textContent += countStr
 }
