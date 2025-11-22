514843f7b578b4e625e3e57a0515fe9cc915b7c1
Whendell <whndllsnts@gmail.com>
2025-11-22 23:36:28 +0800

    Implement Save Workflow: Log current count to history and reset the counter display to zero.

--- JavaScript Fundamentals/index.js
+++ JavaScript Fundamentals/index.js
@@ -3,11 +3,16 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
+    // function when the increment button is clicked will increment counting
     count += 1
     countEl.textContent = count
 }
 
 function save() {
+    // function to display the value in previous entries
     let countStr = count + " - "
     saveEl.textContent += countStr
+            
+    countEl.innerText = 0; // back the count to 0 but continue counting...
+    count = 0;    // back to zero value
 }
