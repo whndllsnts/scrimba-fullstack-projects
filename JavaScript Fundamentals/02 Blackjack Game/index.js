3c70b6f5e5fab5b1a61a31762e3c4499af841fb4
Whendell <whndllsnts@gmail.com>
2025-11-27 21:10:11 +0800

    Implemented the main conditional logic to determine the state of the hand, checking if the sum is less than 21 (draw more), exactly 21 (Blackjack), or over 21 (bust).

--- JavaScript Fundamentals/02 Blackjack Game/index.js
+++ JavaScript Fundamentals/02 Blackjack Game/index.js
@@ -1,3 +1,11 @@
 let firstCard = 6
 let secondCard = 7
 let sum = firstCard + secondCard;
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
