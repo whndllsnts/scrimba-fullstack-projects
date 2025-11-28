c89d29c3540c74689c1738a3a8b559eb2156da9f
Whendell <whndllsnts@gmail.com>
2025-11-29 01:05:40 +0800

    Integrated the card display feature. A new <p> element with the ID cards-el was added to the HTML, and the startGame() function was updated to fetch this element and render the values of firstCard and secondCard directly on the screen.

--- JavaScript Fundamentals/02 Blackjack Game/index.js
+++ JavaScript Fundamentals/02 Blackjack Game/index.js
@@ -6,8 +6,10 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
+let cardsEl = document.getElementById('cards-el')
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
     message = "Do you want to draw a new card?"
