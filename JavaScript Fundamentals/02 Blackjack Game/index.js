7f95e08424dda130760b91901bb3ad67fd9b7cb8
Whendell <whndllsnts@gmail.com>
2025-11-28 09:31:42 +0800

    Refactored the game logic to include the hasBlackJack Boolean variable, which is updated to true when the player hits 21, allowing the program to formally track the win state.

--- JavaScript Fundamentals/02 Blackjack Game/index.js
+++ JavaScript Fundamentals/02 Blackjack Game/index.js
@@ -1,11 +1,16 @@
-let firstCard = 6
-let secondCard = 7
-let sum = firstCard + secondCard;
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
 
 if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
 }
+
+// blackjack
+console.log(hasBlackJack)
