8bc311e09fec9ea56ffc61c559a1dfd786a20d20
Whendell <whndllsnts@gmail.com>
2025-12-01 19:13:09 +0800

    Fully implemented the newCard() function to draw a card, update the sum, and correctly append the new card value to the cards array. The renderGame() function is then called to ensure the game state (sum and message) is instantly updated on the screen.

--- JavaScript Fundamentals/02 Blackjack Game/index.js
+++ JavaScript Fundamentals/02 Blackjack Game/index.js
@@ -5,9 +5,9 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-let messageEl = document.getElementById('message-el')
-let sumEl = document.getElementById('sum-el')
-let cardsEl = document.getElementById('cards-el')
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
     renderGame()
@@ -17,24 +17,22 @@ function renderGame() {
     cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
 
-messageEl.textContent = message
-
-}
 
 function newCard() {
-    console.log("Drawing a new card from the deck!")
-
-    let card = 7
+    let card = 6
     sum += card
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
-
